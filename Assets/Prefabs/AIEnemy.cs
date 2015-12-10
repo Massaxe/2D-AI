83069508bc224401f9554daec6313ee8e11a5173
Massaxe <gabbe.las.2011@gmail.com>
2015-12-10 07:26:47 +0100

    Movement

--- Assets/Prefabs/AIEnemy.cs
+++ Assets/Prefabs/AIEnemy.cs
@@ -7,15 +7,47 @@ public class AIEnemy : MonoBehaviour {
     public bool Fired;
     public float fireRate = 1;
     public Transform player;
-    
-	// Use this for initialization
-	void Start () {
-	
-	}
+    public Vector2 startingPos;
+    public bool moveLeft;
+    public bool moveRight;
+    Rigidbody2D selfR;
+    public float walkDistance;
+    // Use this for initialization
+    void Start () {
+        selfR = GetComponent<Rigidbody2D>();
+
+
+
+    }
+    void Awake()
+    {
+        startingPos = transform.position;
+        moveLeft = true;
+        moveRight = false;
+    }
 	
 	// Update is called once per frame
 	void FixedUpdate () {
-        transform.Translate(new Vector2(0.00003f, 0));
+        if (moveRight)
+        {
+            transform.Translate(Vector2.right * 5 * Time.deltaTime);
+            if (Vector2.Distance(transform.position, startingPos) > walkDistance)
+            {
+                moveRight = false;
+                moveLeft = true;
+            }
+        }
+        if (moveLeft)
+        {
+            transform.Translate(Vector2.left * 5 * Time.deltaTime);
+            if (Vector2.Distance(transform.position, startingPos) > walkDistance)
+            {
+                moveRight = true;
+                moveLeft = false;
+            }
+        }
+        //transform.Translate(new Vector2(0.00003f, 0));
+        print(Vector2.Distance(transform.position, startingPos));
 
 	
 	}
@@ -51,3 +83,6 @@ public class AIEnemy : MonoBehaviour {
         Fired = false;
     }
 }
+
+
+//Vector2.Distance(transform.position, startingPos) > leftDistance
